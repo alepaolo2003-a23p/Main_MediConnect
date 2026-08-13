b8b68aef77207dbd38c8d5aa1a04c7e76c465acd
aleeeeeeeeeeeeeeee <alepaolo.2003@gmail.com>
2026-08-13 00:40:03 -0500

    chore(server): add socket.io server and expose io via app.set('io')

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const app = require("./app");
+const http = require("http");
+const { Server } = require("socket.io");
 const {
     DB_USER,
     DB_HOST,
@@ -16,7 +18,23 @@ const PORT = process.env.PORT || 3977;
 
 mongoose.connect(MONGO_URI)
     .then(() => {
-        app.listen(PORT, () => {
+        const server = http.createServer(app);
+        const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
+
+        // Exponer io para que los controllers lo usen vía req.app.get('io')
+        app.set('io', io);
+
+        io.on('connection', (socket) => {
+            console.log('Socket conectado:', socket.id);
+            socket.on('join', (userId) => {
+                if (userId) {
+                    socket.join(String(userId));
+                    console.log(`Socket ${socket.id} se unió a room ${userId}`);
+                }
+            });
+        });
+
+        server.listen(PORT, () => {
             console.log("##########################");
             console.log("####### MediConnect ######");
             console.log("##########################");
